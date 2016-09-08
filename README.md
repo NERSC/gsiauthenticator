232756cfa262f9cf051e7695e3ca191a1a6331b6
Shreyas Cholia <shreyas@gmail.com>
2016-09-08 11:25:14 -0700

    Add documentation

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 Enables GSI Authentication for Jupyterhub. Acquires an X509 certificate from a myproxy service.
 
-Use with [SSH Spawner]() to authenticate to remote host with GSISSH
+Use with [SSH Spawner](https://github.com/NERSC/SSHSpawner) to authenticate to remote host with GSISSH
+
+## Installation
+
+Requires Python 3
+
+```
+python setup.py install 
+```
+
+## Configuration
+
+See [jupyterhub_config.py](jupyterhub_config.py) for a sample configuration
--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,3 +1,14 @@
+# Sample Configuration
 c.JupyterHub.authenticator_class = 'gsiauthenticator.auth.GSIAuthenticator'
 c.GSIAuthenticator.server = 'nerscca2.nersc.gov'
 c.JupyterHub.cookie_max_age_days = 0.5
+
+# Port for MyProxy Server
+# c.GSIAuthenticator.port = 7512
+
+# Lifetime of Certificate in seconds. This should match the equivalent
+# value in `c.JupyterHub.cookie_max_age_days` eg. 0.5 days => 43200 seconds
+# c.GSIAuthenticator.proxy_lifetime = 43200
+
+# Prefix for writing out cert files. FIle will be <cert_path_prefix><username>
+# c.GSIAuthenticator.cert_path_prefix = '/tmp/x509_'
